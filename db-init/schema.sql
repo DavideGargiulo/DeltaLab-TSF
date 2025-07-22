0ba6eb5803940c9ccc46d7139f78974bcd1cc28c
DavideGargiulo <davide.gargiulo002@gmail.com>
2025-07-22 19:02:18 +0200

    general: added base configuration

---
+++ db-init/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS ACCOUNT (
+    ID CHAR(6) PRIMARY KEY, -- Stringa univoca di 6 caratteri
+    nickname VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL, -- meglio salvarla hashata
+    lingua ENUM(
+        'it', 'en', 'fr', 'de', 'es', 'pt', 'nl', 'sv', 'no', 'da', 'fi', 
+        'pl', 'cs', 'sk', 'hu', 'ro', 'bg', 'el', 'hr', 'sl', 'lt', 'lv', 'et',
+        'ja', -- Giapponese
+        'ru', -- Russo
+        'zh'  -- Cinese (Mandarino)
+    ) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS LOBBY (
+    ID CHAR(6) PRIMARY KEY, -- Stringa univoca di 6 caratteri
+    utenti_connessi INT DEFAULT 0,
+    rotazione ENUM('orario', 'antiorario') NOT NULL,
+    id_accountCreatore CHAR(6) NOT NULL,
+    isPrivate BOOLEAN DEFAULT FALSE,
+    status ENUM('waiting', 'started', 'finished') DEFAULT 'waiting',
+    FOREIGN KEY (id_accountCreatore) REFERENCES ACCOUNT(ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
